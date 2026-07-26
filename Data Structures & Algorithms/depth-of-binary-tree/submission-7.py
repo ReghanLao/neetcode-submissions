9ca21f3c3390f8fafaa559fc43411dd14efef203
Reghan Lao <109181264+ReghanLao@users.noreply.github.com>
2026-07-26 19:45:26 -0400

    Add: depth-of-binary-tree - submission-7

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-7.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        '''
+            Run a DFS from the root node down to every single 
+            leaf node and compute the best possible depth aka the 
+            maximum possible depth from root to leaf 
+        '''
+
+        #by having variables tied to the specific instance of this class
+        #we are able to persist the curr depth at a given level and keep
+        #track of it regardless of what branch we are on 
+        self.max_depth = 0 
+        self.curr_depth = 0
+
+        def dfs(node):
+            #we have finished traversing a path from root to leaf
+            if node is None:
+                self.max_depth = max(self.max_depth, self.curr_depth)
+                return 
+
+            #increments the current depth of the path we are on
+            #if we arrive at a new level
+            self.curr_depth += 1
+
+            dfs(node.left)
+            dfs(node.right)
+
+            #we only want to decrement a level that has been counted 
+            #thus we don't decrement in the base case cuz we don't
+            #count null nodes contributing to a level
+            #backtrack and decrement current depth as we go back up a level
+            self.curr_depth -= 1
+        dfs(root)
+        return self.max_depth 
